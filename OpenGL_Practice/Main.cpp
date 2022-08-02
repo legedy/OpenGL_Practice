5dbc7e308bc069dcf179e0f5ba51812e814ada8e
legedy <legendaryelix3r@gmail.com>
2022-08-02 23:20:37 +1000

    m

--- OpenGL_Practice/Main.cpp
+++ OpenGL_Practice/Main.cpp
@@ -18,39 +18,26 @@ const unsigned int height = 800;
 
 const float ratio = (float)(width / height);
 
-
 GLfloat vertices[] =
-{ //     COORDINATES             COLORS         TexCoord
-	//-0.5f, -0.5f, 0.0f,     1.0f, 0.0f, 0.0f,	0.0f, 0.0f,
-	//-0.5f,  0.5f, 0.0f,     0.0f, 1.0f, 0.0f,	0.0f, 1.0f,
-	// 0.5f,  0.5f, 0.0f,     0.0f, 0.0f, 1.0f,	1.0f, 1.0f,
-	// 0.5f, -0.5f, 0.0f,     1.0f, 1.0f, 1.0f,	1.0f, 0.0f
-
-	-0.5f, -0.5f, 0.5f,		1.0f, 0.0f, 0.0f,	0.0f, 0.0f,
-	-0.5f,  0.5f, 0.5f,		0.0f, 1.0f, 0.0f,	0.0f, 0.0f,
-	 0.5f,  0.5f, 0.5f,		0.0f, 0.0f, 1.0f,	1.0f, 0.0f,
-	 0.5f, -0.5f, 0.5f,		1.0f, 1.0f, 1.0f,	1.0f, 0.0f,
-
-	-0.5f, -0.5f, -0.5f,	1.0f, 0.0f, 0.0f,	0.0f, 1.0f, // Lower left corner
-	-0.5f,  0.5f, -0.5f,	0.0f, 1.0f, 0.0f,	0.0f, 1.0f, // Upper left corner
-	 0.5f,  0.5f, -0.5f,	0.0f, 0.0f, 1.0f,	1.0f, 1.0f,	// Upper right corner
-	 0.5f, -0.5f, -0.5f,	1.0f, 1.0f, 1.0f,	1.0f, 1.0f, // Lower right corner
+{ //     COORDINATES     /        COLORS      /   TexCoord  //
+	-0.5f, 0.0f,  0.5f,     0.83f, 0.70f, 0.44f,	0.0f, 0.0f,
+	-0.5f, 0.0f, -0.5f,     0.83f, 0.70f, 0.44f,	5.0f, 0.0f,
+	 0.5f, 0.0f, -0.5f,     0.83f, 0.70f, 0.44f,	0.0f, 0.0f,
+	 0.5f, 0.0f,  0.5f,     0.83f, 0.70f, 0.44f,	5.0f, 0.0f,
+	 0.0f, 0.8f,  0.0f,     0.92f, 0.86f, 0.76f,	2.5f, 5.0f
 };
 
 GLuint indices[] = {
-	//0, 2, 1,
-	//0, 3, 2,
-
-	//4, 6, 5,
-	//4, 7, 6,
-	
-	0, 4, 5,
-	1, 5, 0,
-
-
+	0, 1, 2,
+	0, 2, 3,
+	0, 1, 4,
+	1, 2, 4,
+	2, 3, 4,
+	3, 0, 4
 };
 
-
+double lastTime = std::clock();
+const double startClock = lastTime;
 
 int main() {
 	glfwInit();
@@ -91,16 +78,25 @@ int main() {
 	Texture poop("kanye.png", GL_TEXTURE_2D, GL_TEXTURE0, GL_RGBA, GL_UNSIGNED_BYTE);
 	poop.texUnit(shaderProgram, "tex0", 0);
 
+	glEnable(GL_DEPTH_TEST);
+
 	while (!glfwWindowShouldClose(window)) {
+		double currentTime = std::clock();
+
+		double deltaTime = (currentTime - lastTime) / 1000;
+		double timePassed = (currentTime - startClock) / 1000;
+		std::cout << (1 / deltaTime) << "\n";
+
 		glClearColor(0.07f, 0.13f, 0.17f, 1.0f);
-		glClear(GL_COLOR_BUFFER_BIT);
+		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 		shaderProgram.Activate();
 
 		glm::mat4 model = glm::mat4(1.0f);
 		glm::mat4 view = glm::mat4(1.0f);
 		glm::mat4 proj = glm::mat4(1.0f);
 
-		model = glm::translate(model, glm::vec3(0, 0, -2));
+		model = glm::translate(model, glm::vec3(0, 0.5f, -1));
+		model = glm::rotate(model, glm::radians((float)(timePassed * 45)), glm::vec3(1,1,1));
 		view = glm::translate(view, glm::vec3(0, -0.5f, -2.0f));
 		proj = glm::perspective(glm::radians(45.0f), ratio, 0.1f, 100.0f);
 
@@ -114,9 +110,11 @@ int main() {
 		glUniform1f(uniID, 0.5f);
 		poop.Bind();
 		VAO.Bind();
-		glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+		glDrawElements(GL_TRIANGLES, sizeof(indices) / sizeof(int), GL_UNSIGNED_INT, 0);
 		glfwSwapBuffers(window);
 		glfwPollEvents();
+
+		lastTime = currentTime;
 	}
 
 	VAO.Delete();
